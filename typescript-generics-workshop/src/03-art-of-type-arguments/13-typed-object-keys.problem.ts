c50662e3b5fbf7b72e91e113abf065a6c071d26a
shura <shyrkatawi@gmail.com>
2026-03-16 23:03:16 +0300

    typescript-generics-workshop: 13

--- typescript-generics-workshop/src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
+++ typescript-generics-workshop/src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
@@ -1,11 +1,6 @@
 import {Equal, Expect} from "../helpers/type-utils";
 
-/**
- * There are two possible solutions to this problem - and it's
- * to do with the way you specify the generic. Can you get
- * both solutions?
- */
-const typedObjectKeys = (obj: unknown) => {
+const typedObjectKeys = <T extends Record<string, any>>(obj: T):  Array<keyof T>=> {
   return Object.keys(obj);
 };
 
@@ -14,7 +9,5 @@ const result1 = typedObjectKeys({
   a: 1,
   b: 2,
 });
-
-expect(result1).toEqual(["a", "b"]);
 
 type test = Expect<Equal<typeof result1, Array<"a" | "b">>>;
