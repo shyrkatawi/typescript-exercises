755be776fd4abb4110c196efe3bf9be0432c8e8b
shura <shyrkatawi@gmail.com>
2026-03-16 22:12:19 +0300

    typescript-generics-workshop: 12

--- typescript-generics-workshop/src/03-art-of-type-arguments/12-generics-at-different-levels.problem.ts
+++ typescript-generics-workshop/src/03-art-of-type-arguments/12-generics-at-different-levels.problem.ts
@@ -1,59 +1,43 @@
 import {Equal, Expect} from "../helpers/type-utils";
 
-export const getHomePageFeatureFlags = (
-  config: unknown,
-  override: (flags: unknown) => unknown
+const EXAMPLE_CONFIG = {
+  apiEndpoint: "https://api.example.com",
+  apiVersion: "v1",
+  apiKey: "1234567890",
+  rawConfig: {
+    featureFlags: {
+      homePage: {
+        showBanner: true,
+        showLogOut: false,
+      },
+      loginPage: {
+        showCaptcha: true,
+        showConfirmPassword: false,
+      },
+    },
+  },
+};
+
+export const getHomePageFeatureFlags = <T extends typeof EXAMPLE_CONFIG>(
+  config: T,
+  override: (flags: T['rawConfig']['featureFlags']['homePage']) => T['rawConfig']['featureFlags']['homePage']
 ) => {
   return override(config.rawConfig.featureFlags.homePage);
 };
 
-describe("getHomePageFeatureFlags", () => {
-  const EXAMPLE_CONFIG = {
-    apiEndpoint: "https://api.example.com",
-    apiVersion: "v1",
-    apiKey: "1234567890",
-    rawConfig: {
-      featureFlags: {
-        homePage: {
-          showBanner: true,
-          showLogOut: false,
-        },
-        loginPage: {
-          showCaptcha: true,
-          showConfirmPassword: false,
-        },
-      },
-    },
-  };
-  it("Should return the homePage flag object", () => {
-    const flags = getHomePageFeatureFlags(
-      EXAMPLE_CONFIG,
-      (defaultFlags) => defaultFlags
-    );
 
-    expect(flags).toEqual({
-      showBanner: true,
-      showLogOut: false,
-    });
+const flags1 = getHomePageFeatureFlags(
+  EXAMPLE_CONFIG,
+  (defaultFlags) => defaultFlags
+);
+type tests1 = [
+  Expect<Equal<typeof flags1, { showBanner: boolean; showLogOut: boolean }>>
+];
 
-    type tests = [
-      Expect<Equal<typeof flags, { showBanner: boolean; showLogOut: boolean }>>
-    ];
-  });
-
-  it("Should allow you to modify the result", () => {
-    const flags = getHomePageFeatureFlags(EXAMPLE_CONFIG, (defaultFlags) => ({
-      ...defaultFlags,
-      showBanner: false,
-    }));
-
-    expect(flags).toEqual({
-      showBanner: false,
-      showLogOut: false,
-    });
-
-    type tests = [
-      Expect<Equal<typeof flags, { showBanner: boolean; showLogOut: boolean }>>
-    ];
-  });
-});
+const flags2 = getHomePageFeatureFlags(EXAMPLE_CONFIG, (defaultFlags) => ({
+  ...defaultFlags,
+  showBanner: false,
+}));
+type tests2 = [
+  Expect<Equal<typeof flags2, { showBanner: boolean; showLogOut: boolean }>>
+];
